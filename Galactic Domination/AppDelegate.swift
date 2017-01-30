b5ec363ddd28690cdfaf7e0d8835ddf9260b079a
Simon Germain <simon.germain@bluesombrero.com>
2017-01-29 19:53:09 -0500

    Initial import

---
+++ Galactic Domination/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Galactic Domination
+//
+//  Created by Simon Germain on 1/29/17.
+//  Copyright © 2017 SquareFrog. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
